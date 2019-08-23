822d97dec7403cdfcc1b12df9c34e2c30e932e2f
Tassan <tassanf@gmail.com>
2019-08-23 07:52:07 -0300

    Sub function

--- main.rs
+++ main.rs
@@ -1,12 +1,16 @@
 fn main() {
     println!("Welcome to The Calculator!");
     println!("Here are some examples of calcs:");
-    println!("Sum -> 2 + 2 = {}", sum(2,2));
-    println!("Sub -> 2 - 2 = {}", 0);
+    println!("Sum -> 2 + 2 = {}", sum(2, 2));
+    println!("Sub -> 2 - 2 = {}", sub(2, 2));
     println!("Mult -> 2 * 2 = {}", 4);
     println!("Div -> 2 / 2 = {}", 1);
 }
 
 fn sum(a: i32, b: i32) -> i32 {
     a+b
+}
+
+fn sub(a: i32, b: i32) -> i32 {
+    a-b
 }
