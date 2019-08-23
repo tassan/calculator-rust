26a75a83d0d2a683a69608b65086bfbf790caf55
Tassan <tassanf@gmail.com>
2019-08-23 07:43:19 -0300

    First Commit

---
+++ main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    println!("Welcome to The Calculator!");
+    println!("Here are some examples of calcs:");
+    println!("Sum -> 2 + 2 = {}", 4);
+    println!("Sub -> 2 - 2 = {}", 0);
+    println!("Mult -> 2 * 2 = {}", 4);
+    println!("Div -> 2 / 2 = {}", 1);
+}
